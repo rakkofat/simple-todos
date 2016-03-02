7ded929c1cbfe901d79a6c7e31264bd585cf61dc
Jonathan Devers <jonathan.devers@gmail.com>
2016-03-01 21:07:27 -0500

    add input form

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,24 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+    }
   });
 }
