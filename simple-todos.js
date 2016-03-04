05c3a51e98e99f518e5500bc0d9f518d67f9effa
Jonathan Devers <jonathan.devers@gmail.com>
2016-03-04 08:17:18 -0500

    add user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -27,7 +27,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -46,5 +48,9 @@ if (Meteor.isClient) {
     "click .delete": function() {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
